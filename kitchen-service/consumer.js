910761270d90b6a9d873812d4871c2854a23c911
KUNDAN KUMAR GUPTA <102321668+kundankrgupta1@users.noreply.github.com>
2026-07-11 17:26:16 +0530

    Add files via upload

---
+++ kitchen-service/consumer.js
@@ -0,0 +1,39 @@
+import { kafka } from "./kafka.js";
+import producer from "./producer.js";
+
+const consumer = kafka.consumer({
+    groupId: "kitchen-group"
+});
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function startConsumer() {
+    await consumer.connect();
+    console.log("✅ Kitchen Consumer Connected");
+
+    await producer.connectProducer();
+
+    await consumer.subscribe({
+        topics: ["order.created"],
+        fromBeginning: false
+    });
+    console.log("📡 Listening for Orders...");
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            const order = JSON.parse(message.value.toString());
+            console.log("\n===============================");
+            console.log("✅ New Order Received");
+            console.log("\n👨‍🍳 Preparing Food...\n");
+            console.log("===============================");
+
+            await delay(5000);
+            await producer.publishFoodReady({
+                ...order,
+                status: 'READY'
+            });
+        },
+    })
+};
+
+startConsumer().catch((e) => console.log(e));
---
+++ kitchen-service/producer.js
@@ -0,0 +1,21 @@
+import { Partitioners } from "kafkajs";
+import { kafka } from "./kafka.js";
+
+const producer = kafka.producer({ createPartitioner: Partitioners.LegacyPartitioner });
+
+async function connectProducer() {
+    await producer.connect();
+    console.log("✅ Kitchen Producer Connected");
+};
+
+async function publishFoodReady(order) {
+    await producer.send({
+        topic: 'food.ready',
+        messages: [{ value: JSON.stringify(order) }]
+    });
+    console.log("🍕 Food is Ready");
+    console.log('ORDER DETAILS:');
+    console.log(order);
+}
+
+export default { connectProducer, publishFoodReady };

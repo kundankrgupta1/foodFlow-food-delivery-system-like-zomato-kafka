b90d7da3a6318af9d9a9561f50c5a534d152f121
KUNDAN KUMAR GUPTA <102321668+kundankrgupta1@users.noreply.github.com>
2026-07-11 17:24:49 +0530

    Create consumer.js

---
+++ delivery-service/consumer.js
@@ -0,0 +1,46 @@
+import { kafka } from "./kafka.js";
+import producer from "./producer.js";
+
+const consumer = kafka.consumer({
+    groupId: 'delivery-group'
+});
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function startConsumer() {
+    await consumer.connect();
+    console.log("✅ Delivery Consumer Connected");
+
+    await producer.connectProducer();
+
+    await consumer.subscribe({
+        topic: ["food.ready"],
+        fromBeginning: false
+    });
+    console.log("🍕 waiting for food ready...");
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            const order = JSON.parse(message.value.toString());
+            if (order?.status === 'READY') {
+
+                await delay(5000); //5s
+                await producer.startProducing({
+                    ...order,
+                    status: 'PICKEDUP',
+                    deliveryPartner: {
+                        name: "Shyam Kumar",
+                        phone: "+91-9883478445"
+                    }
+                });
+
+            }
+
+            await delay(10000); // 10s
+            await producer.publishOrderDelivered({
+                ...order,
+                status: 'DELIVERED'
+            })
+        }
+    })
+}
